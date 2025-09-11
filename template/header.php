27cbaaa84365b12b5eae1b3995f2b22e3f8b244b
Ahmad-Fadhil257 <2zi009adf@gmail.com>
2025-09-11 09:20:35 +0700

    membuat dan menampilkan halaman user

--- template/header.php
+++ template/header.php
@@ -31,9 +31,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-icon">
+                    <i class="fas fa-school"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">BukuTamuKu</div>
             </a>
@@ -43,23 +43,23 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="buku-tamu.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <i class="fas fa-book-open"></i>
                     <span>Buku Tamu</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="laporan.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <i class="fas fa-fw fa-file-alt"></i>
                     <span>Laporan</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="user.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                <a class="nav-link" href="users.php">
+                    <i class="fas fa-users"></i>
                     <span>User</span></a>
             </li>
 
